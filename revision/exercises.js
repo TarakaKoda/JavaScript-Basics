af8d982df43cc7f723ae61897e032e5c63487bca
TarakaKoda <taraka.koda@gmail.com>
2023-10-23 15:53:37 +0530

    Revision Completed.

--- revision/exercises.js
+++ revision/exercises.js
@@ -432,4 +432,51 @@ const circle = {
     }
 }
 console.log(circle.radius);
-console.log(circle.area)
+console.log(circle.area);
+
+//* Prototypical Inheritance.
+function HtmlElement() {
+    this.click = function() {
+        console.log('Clicked');
+    }
+}
+
+HtmlElement.prototype.focus = function() {
+    console.log('Focused');
+}
+
+function HtmlSelectElement(items=[]) {
+    this.items = items;
+
+    this.addItem = function(item) {
+        this.items.push(item);
+    }
+
+    this.removeItem = function(item) {
+        this.items.splice(this.items.indexOf(item), 1);
+    }
+
+    this.render = function() {
+        return `
+    <select>${this.items.map(element => 
+        `<option>${element}</option>`).join('')}
+    </select>`;
+    }
+}
+
+HtmlSelectElement.prototype = new HtmlElement(); 
+HtmlSelectElement.prototype.constructor = HtmlSelectElement;
+
+function HtmlImageElement(src) {
+    this.src = src;
+
+    this.render = function() {
+        return `<img src="${this.src}" />`
+    }
+}
+
+HtmlImageElement.prototype = new HtmlElement();
+HtmlImageElement.prototype.constructor = HtmlImageElement;
+
+const s = new HtmlSelectElement(["1","2", "3", "4"]);
+console.log(s.render());
